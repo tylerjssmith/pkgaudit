95d111cb549991cf969293fa4e64f15330e88250
Tyler Smith <tylerjssmith@gmail.com>
2026-06-26 15:29:56 -0700

    added importFrom statements to resolve check notes

---
+++ R/pkgaudit-package.R
@@ -0,0 +1,18 @@
+#' @keywords internal
+"_PACKAGE"
+
+## usethis namespace: start
+#' @importFrom DBI dbConnect
+#' @importFrom DBI dbDisconnect
+#' @importFrom DBI dbGetQuery
+#' @importFrom openssl base64_decode
+#' @importFrom openssl read_pubkey
+#' @importFrom openssl signature_verify
+#' @importFrom RSQLite SQLite
+#' @importFrom stats setNames
+#' @importFrom xml2 read_xml
+#' @importFrom xml2 xml_attr
+#' @importFrom xml2 xml_find_all
+#' @importFrom xmlparsedata xml_parse_data
+## usethis namespace: end
+NULL
